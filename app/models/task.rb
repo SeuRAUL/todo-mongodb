2225db6ee7c2eda29384b0ae83d0670fd8cd5af0
SeuRaul <seuraul.0@gmail.com>
2021-01-05 14:26:54 -0300

    create tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
-class Todo
+class Task
   include Mongoid::Document
   include Mongoid::Timestamps
   field :task, type: String
   field :done, type: Mongoid::Boolean
   field :date, type: Time
+  belongs_to :user
 end
--- app/views/todos/_todo.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! todo, :id, :task, :done, :date, :created_at, :updated_at
-json.url todo_url(todo, format: :json)
